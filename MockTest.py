40315e968a0110f82983893f6d1586e0f24bd165
Yuxin <yuxinzz212@gmail.com>
2024-08-21 17:09:26 +0800

    MockTest.py

--- MockTest.py
+++ MockTest.py
@@ -1,46 +1,58 @@
-# Get User Input
-Fish_Name1 = input("Enter the name of Fish: ")
-Length1 = float(input(f"Enter the length of the {Fish_Name1}: "))
-Girth1 = float(input(f"Enter the girth of the {Fish_Name1}: "))
+# Define an empty list to store the fish and weight
+fish_data = []
 
-Fish_Name2 = input("Enter the name of the Fish: ")
-Length2 = float(input(f"Enter the length of the {Fish_Name2}: "))
-Girth2 = float(input(f"Enter the girth of the {Fish_Name2}: "))
+# User can input any number for the entries for fishes
+num_of_entries = int(input("Number of entries: "))
 
 # Calculate the weight of the Fish
-def calculate_weight(Length1, Girth1):
-    Weight1 = (Girth1 * Girth1 * Length1) / 800
-    return round(Weight1, 2)
+def calculate_weight(length, girth):
+    weight = (girth * girth * length) / 800
+    return round(weight, 2)
 
-def calculate_weight(Length2, Girth2):
-    Weight2 = (Girth2 * Girth2 * Length2) / 800
-    return round(Weight2, 2)
-
-# Calculate the weight using the Function
-Weight1 = calculate_weight(Length1, Girth1)
-Weight2 = calculate_weight(Length2, Girth2)
+# Create a validation where it prompts the user to key in a valid numeric number for (length, girth) 
+def numeric_value(length_and_girth, length_and_girth_error_message):
+    while True:
+        try:
+            value = float(input(length_and_girth))
+            return value
+        except ValueError:
+            print(length_and_girth_error_message)
 
 # Classify the fish based on the weight
-if Weight1 <= 41:
-    classification1 = ("Small Fish")
-elif Weight1 <= 99:
-    classification1 = ("Medium Fish")
-elif Weight1 <= 174:
-    classification1 = ("Big Fish")
-else:
-    classification1 = ("Giant Fish")
+def categorise_weight(fish):
+    if fish <= 41:
+        return "Small Fish"
+    elif 42 <= fish <= 99:
+        return "Medium Fish"
+    elif 100 <= fish <= 174:
+        return "Big Fish"
+    else:
+        return "Giant Fish"
 
-if Weight2 <= 41:
-    classification2 = ("Small Fish")
-elif Weight2 <= 99:
-    classification2 = ("Medium Fish")
-elif Weight2 <= 174:
-    classification2 = ("Big Fish")
-else:
-    classification2 = ("Giant Fish")
+# Calculate the weight of the Fish
+def calculate_weight(length, girth):
+    weight = (girth * girth * length) / 800
+    return round(weight, 2)
 
-print(f"The weight of the fish for {Fish_Name1} is: {Weight1} pounds")
-print(f"The fish is classified as: {classification1}")
+# Prompt the User input the name of the fish, length and girth of the fish.
+# If invalid input is entered for length and girth it will ask the user to re-enter again.
+for i in range(num_of_entries):
+    fish_name = input(f"Enter the name of the Fish {i+1}: ")
+    length = numeric_value(f"Enter the length of {fish_name}: ", "Invalid input, please re-enter a numeric value for length.")
+    girth = numeric_value(f"Enter the girth of {fish_name}: ", "Invalid input, please re-enter a numeric value for girth.")
 
-print(f"The weight of the fish for {Fish_Name2} is {Weight2} pounds")
-print(f"The fish is classified as: {classification2}")
+    weight = calculate_weight(length, girth)
+    classification = categorise_weight(weight)
+
+    fish_data.append({
+        "Fish Name": fish_name,
+        "Weight(pounds)": weight,
+        "Classification": classification
+    })
+    
+    # Display the fish data
+    print("\nFish Data:")
+    print(f"The name of the Fish is: {fish_name}")
+    print(f"The weight of the Fish is: {weight} pounds")
+    print(f"The Fish is classified as: {classification}")
+    print("")
